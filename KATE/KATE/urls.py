6077165771bc17dfaeb2b26bf1945c61cd05d2fa
Yifan Wu <yw8012@ic.ac.uk>
2016-11-18 17:01:25 +0000

    create kateapp

--- KATE/KATE/urls.py
+++ KATE/KATE/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""KATE URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^$', include('kateapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
---
+++ KATE/kateapp/models.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class People(models.Model):
+    login = models.C

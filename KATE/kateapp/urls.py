829d764c895603a062e1b6e1e2cbe55e4e032ef9
Yifan Wu <yw8012@ic.ac.uk>
2016-11-19 15:49:41 +0000

    add period

--- KATE/kateapp/urls.py
+++ KATE/kateapp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'kateapp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^timetable/2016/(?P<period_id>[1-7])/(?P<letter_yr>[a-z][0-9])/(?P<login>[a-z0-9]+)/$', views.timetable, name='timetable'),
 ]
--- KATE/kateapp/views.py
+++ KATE/kateapp/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Classes, People, Courses, Term, Courses_Term, Courses_Classes, Exercises, Period
+
 def index(request):
     return render(request, 'kateapp/home.html')
+
+def timetable(request, period_id, letter_yr, login):
+    period = get_object_or_404(Period, pk=period_id)
+    classes = get_object_or_404(Classes, pk=letter_yr)
+    people = get_object_or_404(People, pk=login)
+    return render(request, 'kateapp/timetable.html', {'period': period, 'classes': classes, 'people': people})

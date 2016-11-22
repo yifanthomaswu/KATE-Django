6de60277fcf1935170bd1c12d7f7eb8de356351c
Daniel Budiono <danielbudiono@dyn1204-18.wlan.ic.ac.uk>
2016-11-22 16:47:24 +0000

    Add styling for exercise_setup template

--- KATE/kateapp/forms.py
+++ KATE/kateapp/forms.py
@@ -2,16 +2,16 @@ from django import forms
 from .models import Exercises
 
 class NewExerciseForm(forms.Form):
-    exercise = forms.CharField(label='Exercise Title', max_length=200)
+    exercise = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}) )
     start_date = forms.DateTimeField(widget=forms.TextInput(attrs=
                                 {
-                                    'class':'datepicker'
+                                    'class':'datepicker form-control'
                                 }))
     end_date = forms.DateTimeField(widget=forms.TextInput(attrs=
                                 {
-                                    'class':'datepicker'
+                                    'class':'datepicker form-control'
                                 }))
-    number = forms.IntegerField()
+    number = forms.IntegerField(widget=forms.NumberInput(attrs={'class' : 'form-control'}))
     exercise_type = forms.ChoiceField(choices=Exercises.TYPE_CHOICES)
     assessment = forms.ChoiceField(choices=Exercises.ASSESSMENT_CHOICES)
     submission = forms.ChoiceField(choices=Exercises.SUBMISSION_CHOICES)

64f398a0223c7f014f581792615069b81711bba7
cc6313 <cc6313@ic.ac.uk>
2016-11-29 16:49:46 +0000

    change login config

--- KATE/KATE/settings.py
+++ KATE/KATE/settings.py
@@ -136,7 +136,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 AUTH_LDAP_SERVER_URI = "ldaps://ldaps-vip.cc.ic.ac.uk:636"
-AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,OU=Users,OU=Imperial College (London),DC=ic,DC=ac,DC=uk"
+AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,OU=doc,OU=Users,OU=Imperial College (London),DC=ic,DC=ac,DC=uk"
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/

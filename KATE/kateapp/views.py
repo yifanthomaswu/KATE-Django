3edcd18beedcc4d48a1ded7cd6670085775590a8
Bjarne <bp2214@ic.ac.uk>
2016-12-01 12:03:13 +0000

    individual record basic functionality implemented, todo: css-ing

--- KATE/kateapp/views.py
+++ KATE/kateapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from django.db.models import Max
 
-from .models import Classes, People, Courses, Term, Courses_Term, Courses_Classes, Exercises, Period, Resource, Exercises_Resource, Courses_Resource
+from .models import Classes, People, Courses, Term, Courses_Term, Courses_Classes, Exercises, Period, Resource, Exercises_Resource, Courses_Resource, Marks
 from .forms import NewExerciseForm, SubmissionForm
 
 import datetime
@@ -33,12 +33,35 @@ def personal_page(request):
 
 def individual_record(request, login):
     person = get_object_or_404(People, login=login)
+    courses_marks = []
+    for course in person.registered_courses.all():
+        marks = list(Marks.objects.filter(login=login, exercise__code=course.code).order_by('exercise__number'))
+        textual_marks = [convert_mark_number_text(elem) for elem in marks]
+        courses_marks.append((course, textual_marks))
     context = {
         'person': person,
+        'courses_marks': courses_marks,
     }
-    all_marks = Marks.objects.filter(login=login).order_by('exercise__code', 'exercise__number')
     return render(request, 'kateapp/individual_record.html', context)
-
+def convert_mark_number_text(mark):
+    number_mark = mark.mark
+    if number_mark < 30:
+        mark.mark = 'F'
+    elif number_mark < 40:
+        mark.mark = 'E'
+    elif number_mark < 50:
+        mark.mark = 'D'
+    elif number_mark < 60:
+        mark.mark = 'C'
+    elif number_mark < 70:
+        mark.mark = 'B'
+    elif number_mark < 80:
+        mark.mark = 'A'
+    elif number_mark < 90:
+        mark.mark = 'A+'
+    else:
+        mark.mark = 'A*'
+    return mark
 
 def timetable(request, period_id, letter_yr, login):
     period = get_object_or_404(Period, pk=period_id)

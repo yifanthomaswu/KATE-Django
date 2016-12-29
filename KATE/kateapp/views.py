79319e35fbfb23476b65169d894006f893e55d6d
Bjarne <bp2214@ic.ac.uk>
2016-12-29 13:10:00 +0100

    readded code that went missing

--- KATE/kateapp/views.py
+++ KATE/kateapp/views.py
@@ -32,7 +32,7 @@ def personal_page(request):
     login = "yw8012"
     person = get_object_or_404(People, login=login)
     #courses = list(Courses.objects.filter(required=person)) + list(Courses.objects.filter(registered=person))
-    courses = list(Courses.objects.all()) 
+    courses = list(Courses.objects.all())
     courses_exercises = []
     date_now = timezone.now()
     for course in courses:
@@ -50,6 +50,37 @@ def personal_page(request):
     }
     return render(request, 'kateapp/personal_page.html', context)
 
+def individual_record(request, login):
+    person = get_object_or_404(People, login=login)
+    courses_marks = []
+    for course in person.registered_courses.all():
+        marks = list(Marks.objects.filter(login=login, exercise__code=course.code).order_by('exercise__number'))
+        textual_marks = [convert_mark_number_text(elem) for elem in marks]
+        courses_marks.append((course, textual_marks))
+    context = {
+        'person': person,
+        'courses_marks': courses_marks,
+    }
+    return render(request, 'kateapp/individual_record.html', context)
+def convert_mark_number_text(mark):
+    number_mark = mark.mark
+    if number_mark < 30:
+        mark.mark = 'F'
+    elif number_mark < 40:
+        mark.mark = 'E'
+    elif number_mark < 50:
+        mark.mark = 'D'
+    elif number_mark < 60:
+        mark.mark = 'C'
+    elif number_mark < 70:
+        mark.mark = 'B'
+    elif number_mark < 80:
+        mark.mark = 'A'
+    elif number_mark < 90:
+        mark.mark = 'A+'
+    else:
+        mark.mark = 'A*'
+    return mark
 
 def timetable(request, period_id, letter_yr, login):
     period = get_object_or_404(Period, pk=period_id)

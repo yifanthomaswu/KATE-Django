082a16af7d645333b29c66f0a6e8dcde5accae11
Max Dudzinski <md3414@ic.ac.uk>
2017-01-02 17:53:49 +0000

    u

--- KATE/kateapp/urls.py
+++ KATE/kateapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^timetable/2016/(?P<period_id>[1-7])/(?P<letter_yr>[a-z][0-9])/(?P<login>[a-z0-9]+)/$', views.timetable, name='timetable'),
     url(r'^course/2016/(?P<letter_yr>[a-z][0-9])/$', views.course_list, name='course_list'),
     url(r'^course/2016/(?P<code>[.0-9]+)/$', views.course, name='course'),
-    url(r'^exercise_setup/2016/(?P<code>[.0-9]+)/(?P<number>[1-9]+)/$', views.exercise_setup, name='exercise_setup'),
-    url(r'^submission/2016/(?P<code>[.0-9]+)/(?P<number>[1-9]+)/$', views.submission, name='submission'),
+    url(r'^exercise_setup/2016/(?P<code>[.0-9]+)/(?P<number>[1-9][0-9]*)/$', views.exercise_setup, name='exercise_setup'),
+    url(r'^submission/2016/(?P<code>[.0-9]+)/(?P<number>[1-9][0-9]*)/$', views.submission, name='submission'),
     url(r'^grading_scheme/$', views.grading_scheme, name='grading_scheme'),
 ]
